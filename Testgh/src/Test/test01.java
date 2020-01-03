037035e4d65f0418f5b4ca29a57b4b4c3237a1b9
Administrator <Administrator@SC-201908291516>
2020-01-03 11:45:15 +0800

    1

--- Testgh/src/Test/test01.java
+++ Testgh/src/Test/test01.java
@@ -3,6 +3,7 @@ package Test;
 public class test01 {
 	public static void main(String[] args) {
 		int a = 1;
-                String zh = "hanhan"
+		String zh = "hanhan";
+		String gr = "thh";
 	}
 }
